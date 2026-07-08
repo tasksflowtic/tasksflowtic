d0ec3540b75b45df9a29bc15874b7532f17d5b09
karnelkayth <166887932+karnelkayth@users.noreply.github.com>
2026-07-08 08:40:12 +0530

    cors url updt

--- Server/Index.js
+++ Server/Index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://tasksflowtic.vercel.app',
+    origin: 'https://taskflowtic.vercel.app',
     credentials: true,
     methods: ['GET', 'PUT', 'PATCH', 'DELETE']
 }
