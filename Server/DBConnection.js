7ad0c5339f8592a1b776fb9ab5e4ef13cab4992a
karnelkayth <166887932+karnelkayth@users.noreply.github.com>
2026-07-28 11:55:23 +0530

    dbchnage

--- Server/DBConnection.js
+++ Server/DBConnection.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 
 const dbconnect = async () => {
-    try {
-        const connect = await mongoose.connect(`${process.env.MONGODB_URL}`)
-        console.log('DB Connected')
-    } catch (error) {
-        console.log(error)
-    }
+    // try {
+    //     const connect = await mongoose.connect(`${process.env.MONGODB_URL}`)
+    //     console.log(`DB Connected ${process.env.MONGODB_URL}`)
+    // } catch (error) {
+    //     console.log(error)
+    // }
 }
 module.exports = dbconnect
